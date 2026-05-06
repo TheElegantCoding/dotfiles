ae43865cb0fec2185ba0ed0a9810578c97c2c68c
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-05 20:44:49 -0400

    feat: add Zsh aliases for common Git commands and utilities

---
+++ src/module/zsh/module/aliases.zsh
@@ -0,0 +1,11 @@
+alias ls='colorls --group-directories-first --almost-all'
+alias update="source ~/.zshrc"
+alias gd='git diff'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gu='git pull'
+alias gl='git log --all --graph --decorate --pretty="%C(cyan)%h %C(white) %an %ar%C(auto) %D%n%s%n"'
+alias gb='git branch'
+alias gi='git init'
+alias gcl='git clone'
