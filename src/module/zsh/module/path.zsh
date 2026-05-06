e8a3b1113f95ee3307307c40898874e2c3daf91a
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-05 20:44:33 -0400

    feat: add initial path configuration for Zsh environment

---
+++ src/module/zsh/module/path.zsh
@@ -0,0 +1,14 @@
+export PATH="/ucrt64/bin:/usr/bin:$PATH"
+export PATH="$PATH:/c/Progra~1/starship/bin"
+export PATH="$PATH:/c/Progra~1/nodejs"
+export PATH="$PATH:/c/Progra~1/Git/cmd"
+export PATH="$PATH:/c/Users/luism/AppData/Local/pnpm"
+export PATH="$PATH:/c/Users/luism/.bun/bin"
+export PATH="$PATH:/c/ProgramData/chocolatey/lib/terraform/tools"
+
+export GEM_HOME="/home/luism/.local/share/gem/ruby/3.4.0"
+export PATH="/home/luism/.local/share/gem/ruby/3.4.0/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
