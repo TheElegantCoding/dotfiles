dc201114b49d00553b014a5cdb799f87dfd3d521
TheElegantCoding <luismonsalve1941@gmail.com>
2026-09-25 16:57:50 -0400

    chore: uncomment partition and base script sources, restore success message

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source "$DIR/src/module/install/lib.sh"
-# source "$DIR/src/module/install/partition.sh"
-# source "$DIR/src/module/install/base.sh"
+source "$DIR/src/module/install/partition.sh"
+source "$DIR/src/module/install/base.sh"
 source "$DIR/src/module/install/arch.sh"
 
-
-# success "¡Instalación completada con éxito!"
-# info "Desmonta y reinicia con: umount -R /mnt && reboot"
+success "Arch installation completed successfully."
