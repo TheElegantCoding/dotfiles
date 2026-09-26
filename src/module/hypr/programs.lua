728321972ffde094146c4486825262e00f10be91
TheElegantCoding <luismonsalve1941@gmail.com>
2026-09-25 21:41:01 -0400

    feat: add initial Hyprland configuration files for autostart, environment, input, keybindings, and styling

---
+++ src/module/hypr/programs.lua
@@ -0,0 +1,9 @@
+local terminal    = "konsole"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
+
+return {
+  terminal    = terminal,
+  fileManager = fileManager,
+  menu        = menu,
+}
