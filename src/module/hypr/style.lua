93ca348c859ca2e993b99ef79013f575e3b92c61
TheElegantCoding <luismonsalve1941@gmail.com>
2026-09-25 22:38:04 -0400

    fix: change 'easy' spring curve to bezier type for consistency in animations

--- src/module/hypr/style.lua
+++ src/module/hypr/style.lua
@@ -47,12 +47,12 @@ hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},    {0.75, 1
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
 
 -- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 238.1191, damping = 24.21 })
+hl.curve("easy",           { type = "bezier", mass = 1, stiffness = 238.1191, damping = 24.21 })
 
-hl.animation({ leaf = "global",        enabled = true,  speed = 10,    bezier = "default" })
+hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
+hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, bezier = "easy" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  bezier = "easy",         style = "popin 87%" })
 hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
 hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
 hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
